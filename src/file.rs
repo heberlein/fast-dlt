60d503707ddec17fc6f721510daa4b74e17d04d9
heberlein <heberlein@proton.me>
2022-11-18 18:55:51 +0100

    make DltFile iterator

--- src/file.rs
+++ src/file.rs
@@ -1,27 +1,34 @@
-use crate::{DltFilter, DltMessage, Result};
+use fallible_iterator::FallibleIterator;
+
+use crate::{DltError, DltFilter, DltMessage, Result};
 
 #[derive(Debug)]
 pub struct DltFile<'a> {
-    pub messages: Vec<DltMessage<'a>>,
+    buf: &'a [u8],
+    offset: usize,
 }
 
 impl<'a> DltFile<'a> {
-    pub fn new(buf: &'a [u8]) -> Result<Self> {
-        let mut offset = 0;
-        let mut messages = vec![];
-        loop {
-            if offset >= buf.len() {
-                break;
-            }
-            let message = DltMessage::new(&buf[offset..])?;
-            offset += message.num_bytes();
-            messages.push(message);
-        }
-
-        Ok(DltFile { messages })
+    pub fn new(buf: &'a [u8]) -> Self {
+        Self { buf, offset: 0 }
     }
 
     pub fn filtered(buf: &'a [u8], filter: DltFilter) -> Self {
         todo!()
+    }
+}
+
+impl<'a> FallibleIterator for DltFile<'a> {
+    type Item = DltMessage<'a>;
+    type Error = DltError;
+
+    fn next(&mut self) -> Result<Option<DltMessage<'a>>> {
+        if self.offset >= self.buf.len() {
+            Ok(None)
+        } else {
+            let message = DltMessage::new(&self.buf[self.offset..])?;
+            self.offset += message.num_bytes();
+            Ok(Some(message))
+        }
     }
 }
