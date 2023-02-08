17598ba4a37bf3be2edee8104ef11f6e2e99777e
heberlein <heberlein@proton.me>
2023-02-08 21:25:24 +0100

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,5 @@
 use thiserror::Error;
 
-use crate::ArgumentKind;
-
 #[derive(Debug, Error)]
 pub enum DltError {
     #[error("Missing DLT pattern!")]
@@ -10,8 +8,6 @@ pub enum DltError {
     BadUTF8(#[from] std::str::Utf8Error),
     #[error("Failed to convert slice!")]
     BadSliceConvert(#[from] std::array::TryFromSliceError),
-    #[error("Mismatched Argument kind: expected {0}, got {1}")]
-    KindMismatch(ArgumentKind, ArgumentKind),
 }
 
 pub type Result<T> = std::result::Result<T, DltError>;
