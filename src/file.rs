33b975f34ab575a6054d2fb91c4fa8382aef109b
heberlein <heberlein@proton.me>
2023-02-08 21:22:16 +0100

    impl IntoIterator for DltFile

--- src/file.rs
+++ src/file.rs
@@ -1,7 +1,8 @@
-use fallible_iterator::FallibleIterator;
+use fallible_iterator::{FallibleIterator, Iterator};
 
 use crate::{DltError, DltMessage, Result};
 
+const MIN_MESSAGE_LENGTH: usize = 16 /*Storage Header*/ + 4 /*Smallest Standard Header, no Extended Header */;
 #[derive(Debug)]
 pub struct DltFile<'a> {
     buf: &'a [u8],
@@ -11,6 +12,16 @@ pub struct DltFile<'a> {
 impl<'a> DltFile<'a> {
     pub fn new(buf: &'a [u8]) -> Self {
         Self { buf, offset: 0 }
+    }
+}
+
+impl<'a> IntoIterator for DltFile<'a> {
+    type Item = Result<DltMessage<'a>>;
+
+    type IntoIter = fallible_iterator::Iterator<Self>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iterator()
     }
 }
 
@@ -27,4 +38,15 @@ impl<'a> FallibleIterator for DltFile<'a> {
             Ok(Some(message))
         }
     }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.buf[self.offset..].len() / MIN_MESSAGE_LENGTH))
+    }
 }
+
+#[allow(unused)]
+struct AllMessages {}
+#[allow(unused)]
+struct WithEcuId {}
+#[allow(unused)]
+struct WithAppId {}
