0b4d9164ac7cc71f880f114cca3a89e1ed893256
heberlein <heberlein@proton.me>
2023-02-13 18:49:58 +0100

    add example print_file

---
+++ examples/print_file.rs
@@ -0,0 +1,18 @@
+use std::{error::Error, path::PathBuf};
+
+use fallible_iterator::FallibleIterator;
+use fast_dlt::DltFile;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let Some(path) = std::env::args().nth(1).map(PathBuf::from) else {
+      return Err("This example expects a path to a DLT file!".into());  
+    };
+
+    let data = std::fs::read(path)?;
+
+    let file = DltFile::new(&data);
+
+    file.for_each(|msg| Ok(println!("{msg}")))?;
+
+    Ok(())
+}
